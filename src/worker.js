a4ca8a10aadfe8a1cdaca46add2f2a3a76b62754
Mathieu Mescam <mate@e.email>
2024-07-11 11:36:42 +0200

    Update src/worker.js to handle POST request

--- src/worker.js
+++ src/worker.js
@@ -9,8 +9,33 @@
  */
 
 
-export default {
-  async fetch(request, env, ctx) {
-    return new Response('YOLO');
-  },
-};
+// export default {
+//   async fetch(request, env, ctx) {
+//     return new Response('YOLO');
+//   },
+// };
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  if (request.method === 'POST') {
+    const contentType = request.headers.get('content-type') || '';
+    if (contentType.includes('application/json')) {
+      const json = await request.json();
+      return new Response(JSON.stringify(json), { status: 200 });
+    } else if (contentType.includes('application/x-www-form-urlencoded')) {
+      const formData = await request.formData();
+      const formEntries = {};
+      for (const entry of formData.entries()) {
+        formEntries[entry[0]] = entry[1];
+      }
+      return new Response(JSON.stringify(formEntries), { status: 200 });
+    } else {
+      return new Response('Unsupported Content-Type', { status: 400 });
+    }
+  } else {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+}
